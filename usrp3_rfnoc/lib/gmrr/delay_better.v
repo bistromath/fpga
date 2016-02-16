3b00ac2c81bb8b9eadba4ef1664a0a8296da024c
Nick Foster <bistromath@gmail.com>
2016-02-15 21:34:02 -0800

    Add delay block and testbench. Seems to work.

---
+++ usrp3_rfnoc/lib/gmrr/delay_better.v
@@ -0,0 +1,90 @@
+//
+// Copyright 2017 Nick Foster
+//
+
+module delay_better
+  #(parameter MAX_LEN_LOG2=10,
+    parameter WIDTH=16)
+   (input clk, input reset, input clear,
+    input [MAX_LEN_LOG2-1:0] len,
+    input [MAX_LEN_LOG2-1:0] max_spp,
+    input [WIDTH-1:0] i_tdata, input i_tlast, input i_tvalid, output i_tready,
+    output [WIDTH-1:0] o_tdata, output o_tlast, output o_tvalid, input o_tready);
+
+   reg [MAX_LEN_LOG2-1:0] delay_count;
+   reg [WIDTH-1:0] last_sample;
+   reg [MAX_LEN_LOG2-1:0] spp_count;
+
+   localparam STATE_WAITING_FOR_FIRST_INPUT = 0;
+   localparam STATE_ADVANCE = 1;
+   localparam STATE_DELAY = 2;
+   localparam STATE_RUNNING = 3;
+   reg [1:0] state;
+
+   //how to handle tlast? you could conceivably produce a massive fuck-all packet if your delay is large enough.
+   //instead, let's make it a settings register so that this block also resizes packets coming in.
+   //you could also just have a "maximum SPP" input so that tlast is preserved except during delay events.
+   //TODO use spp_count
+   always @(posedge clk) begin
+     if(reset|clear) begin
+       delay_count <= 0;
+       state <= STATE_WAITING_FOR_FIRST_INPUT;
+       last_sample <= 0;
+       spp_count <= 0;
+     end
+     else
+       case(state)
+         STATE_WAITING_FOR_FIRST_INPUT: begin
+           if(i_tvalid & o_tready) begin
+             last_sample <= i_tdata;
+             if(len>0)
+               state <= STATE_DELAY;
+             else
+               state <= STATE_RUNNING;
+           end
+         end
+         STATE_ADVANCE: begin //advance means we're consuming, but not producing.
+           if(delay_count <= len)
+             state <= STATE_RUNNING;
+           else
+             if(i_tvalid & o_tready) begin
+               delay_count <= delay_count - 1;
+               last_sample <= i_tdata;
+           end
+         end
+         STATE_DELAY: begin // delay means we're producing, but not consuming.
+           if(delay_count >= len)
+             state <= STATE_RUNNING;
+           else
+             if(o_tvalid & o_tready)
+               delay_count <= delay_count + 1;
+         end
+         STATE_RUNNING: begin
+           if(delay_count > len)
+             state <= STATE_ADVANCE;
+           else if(delay_count < len)
+             state <= STATE_DELAY;
+           if(i_tvalid & o_tready)
+             last_sample <= i_tdata;
+         end
+       endcase
+   end
+
+   //i_tready: we're ready to take input:
+   //  IF o_tready AND STATE_RUNNING
+   //  OR STATE_ADVANCE
+   //  OR STATE_WAITING_FOR_FIRST_INPUT
+
+   //o_tvalid: our data is valid on the output:
+   //  IF i_tvalid AND STATE_RUNNING
+   //  OR STATE_DELAY
+
+   assign o_tdata = (state == STATE_DELAY) ? last_sample : i_tdata;
+   assign o_tlast = (state == STATE_DELAY) ? 1'b0 : i_tlast; //FIXME
+   assign o_tvalid = (i_tvalid & (state == STATE_RUNNING))
+                   | (state == STATE_DELAY);
+   assign i_tready = (o_tready & (state == STATE_RUNNING))
+                   | (state == STATE_ADVANCE)
+                   | (state == STATE_WAITING_FOR_FIRST_INPUT);
+
+endmodule // delay_better
---
+++ usrp3_rfnoc/lib/gmrr/noc_block_delay_tb/noc_block_delay_tb.sv
@@ -0,0 +1,141 @@
+`timescale 1ns/1ps
+`define NS_PER_TICK 1
+`define NUM_TEST_CASES 6
+
+`include "sim_exec_report.vh"
+`include "sim_clks_rsts.vh"
+`include "sim_rfnoc_lib.svh"
+
+module noc_block_delay_tb();
+  `TEST_BENCH_INIT("noc_block_delay",`NUM_TEST_CASES,`NS_PER_TICK);
+  localparam BUS_CLK_PERIOD = $ceil(1e9/166.67e6);
+  localparam CE_CLK_PERIOD  = $ceil(1e9/200e6);
+  localparam NUM_CE         = 1;  // Number of Computation Engines / User RFNoC blocks to simulate
+  localparam NUM_STREAMS    = 1;  // Number of test bench streams
+  `RFNOC_SIM_INIT(NUM_CE, NUM_STREAMS, BUS_CLK_PERIOD, CE_CLK_PERIOD);
+  `RFNOC_ADD_BLOCK(noc_block_delay, 0);
+
+  localparam SPP = 600; // Samples per packet
+
+  /********************************************************
+  ** Verification
+  ********************************************************/
+  initial begin : tb_main
+    string s;
+    logic [31:0] idelay, qdelay;
+    logic [63:0] readback;
+
+    /********************************************************
+    ** Test 1 -- Reset
+    ********************************************************/
+    `TEST_CASE_START("Wait for Reset");
+    while (bus_rst) @(posedge bus_clk);
+    while (ce_rst) @(posedge ce_clk);
+    `TEST_CASE_DONE(~bus_rst & ~ce_rst);
+
+    /********************************************************
+    ** Test 2 -- Check for correct NoC IDs
+    ********************************************************/
+    `TEST_CASE_START("Check NoC ID");
+    // Read NOC IDs
+    tb_streamer.read_reg(sid_noc_block_delay, RB_NOC_ID, readback);
+    $display("Read LOOPBACKSPLIT NOC ID: %16x", readback);
+    `ASSERT_ERROR(readback == noc_block_delay.NOC_ID, "Incorrect NOC ID");
+    `TEST_CASE_DONE(1);
+
+    /********************************************************
+    ** Test 3 -- Connect RFNoC blocks
+    ********************************************************/
+    `TEST_CASE_START("Connect RFNoC blocks");
+    `RFNOC_CONNECT(noc_block_tb,noc_block_delay,SC16,SPP);
+    `RFNOC_CONNECT(noc_block_delay,noc_block_tb,SC16,SPP);
+    `TEST_CASE_DONE(1);
+
+    /********************************************************
+    ** Test 4 -- Write / readback user registers
+    ********************************************************/
+    `TEST_CASE_START("Write / readback user registers");
+    idelay = 32'd32;
+    qdelay = 32'd16;
+    tb_streamer.write_user_reg(sid_noc_block_delay, noc_block_delay.SR_DELAY_I, idelay);
+    tb_streamer.read_user_reg(sid_noc_block_delay, 0, readback);
+    $sformat(s, "User register 0 incorrect readback! Expected: %0d, Actual %0d", readback[31:0], idelay);
+    `ASSERT_ERROR(readback[31:0] == idelay, s);
+    tb_streamer.write_user_reg(sid_noc_block_delay, noc_block_delay.SR_DELAY_Q, qdelay);
+    `TEST_CASE_DONE(1);
+
+    /********************************************************
+    ** Test 5 -- Test sequence
+    ********************************************************/
+    `TEST_CASE_START("Test sequence");
+    fork
+      begin
+        cvita_payload_t send_payload;
+        for (int i = 0; i < SPP/2; i++) begin
+          send_payload.push_back({16'(i*2), 16'(i*2), 16'(i*2+1), 16'(i*2+1)});
+        end
+        tb_streamer.send(send_payload);
+      end
+      begin
+        cvita_payload_t recv_payload;
+        cvita_metadata_t md;
+        logic [63:0] expected_value;
+        integer expected_i0, expected_q0;
+        $display("Receiving on DATA port");
+        tb_streamer.recv(recv_payload,md, 0);
+        for (int i = 0; i < SPP/2; i++) begin
+          expected_i0 = i*2-idelay;
+          expected_q0 = i*2-qdelay;
+          expected_value = {expected_i0 < 0 ? 16'b0 : 16'(expected_i0),
+                            expected_q0 < 0 ? 16'b0 : 16'(expected_q0),
+                            (expected_i0+1) < 0 ? 16'b0 : 16'(expected_i0+1),
+                            (expected_q0+1) < 0 ? 16'b0 : 16'(expected_q0+1)};
+          $sformat(s, "Incorrect value received! Expected: %0x, Received: %0x", expected_value, recv_payload[i]);
+          `ASSERT_ERROR(recv_payload[i] == expected_value, s);
+        end
+      end
+    join
+    `TEST_CASE_DONE(1);
+
+    /********************************************************
+    ** Test 5 -- Un-delay everything, test advance.
+    **           Note that, because of the way tlast
+    **           was handled, we got the whole packet
+    **           last time (i.e., SPP>600). So this time
+    **           there's nothing left in the pipeline.
+    ********************************************************/
+    `TEST_CASE_START("Test sequence -- undelay");
+    tb_streamer.write_user_reg(sid_noc_block_delay, noc_block_delay.SR_DELAY_I, 32'd0);
+    tb_streamer.write_user_reg(sid_noc_block_delay, noc_block_delay.SR_DELAY_Q, 32'd0);
+    fork
+      begin
+        cvita_payload_t send_payload;
+        for (int i = 0; i < SPP/2; i++) begin
+          send_payload.push_back({16'(i*2), 16'(i*2), 16'(i*2+1), 16'(i*2+1)});
+        end
+        tb_streamer.send(send_payload);
+      end
+      begin
+        cvita_payload_t recv_payload;
+        cvita_metadata_t md;
+        logic [63:0] expected_value;
+        integer expected_i0, expected_q0;
+        $display("Receiving on DATA port");
+        tb_streamer.recv(recv_payload,md, 0);
+        for (int i = 0; i < SPP/2; i++) begin
+          expected_i0 = i*2+idelay;
+          expected_q0 = i*2+qdelay;
+          expected_value = {16'(expected_i0),
+                            16'(expected_q0),
+                            16'(expected_i0+1),
+                            16'(expected_q0+1)};
+          $sformat(s, "Incorrect value received! Expected: %0x, Received: %0x", expected_value, recv_payload[i]);
+          `ASSERT_ERROR(recv_payload[i] == expected_value, s);
+        end
+      end
+    join
+    `TEST_CASE_DONE(1);
+    `TEST_BENCH_DONE;
+
+  end
+endmodule
