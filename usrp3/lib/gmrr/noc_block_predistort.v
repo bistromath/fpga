af5d60d5d2bf822bf27f0a2786855d53007da3ff
Nick Foster <bistromath@gmail.com>
2016-05-06 00:25:56 -0700

    Interim commit while simulating. Sim works.

--- usrp3/lib/gmrr/noc_block_predistort.v
+++ usrp3/lib/gmrr/noc_block_predistort.v
@@ -4,8 +4,8 @@
 
 module noc_block_predistort #(
   parameter NOC_ID = 64'h6275_7474_7300_0000,
-  parameter STR_SINK_FIFOSIZE = 11,
-  parameter NUM_CHANNELS = 4)
+  parameter NUM_CHANNELS = 1,
+  parameter STR_SINK_FIFOSIZE = 11)
 (
   input bus_clk,
   input bus_rst,
@@ -41,9 +41,6 @@ module noc_block_predistort #(
   // Settings registers addresses
   localparam SR_NEXT_DST    = 128;
   localparam SR_AXI_CONFIG  = 129;
-  //parameter for setting which channel you're trying to load taps for.
-  //this is not the best idea but i don't know how else to do it.
-  localparam SR_WHICH_TAPS  = 192;
   localparam SR_READBACK    = 255;
 
 
@@ -61,7 +58,7 @@ module noc_block_predistort #(
   // RFNoC Shell
   wire [31:0]             set_data;
   wire [7:0]              set_addr;
-  wire [NUM_CHANNELS-1:0] set_stb;
+  wire                    set_stb;
 
   wire [63:0]   cmdout_tdata, ackin_tdata;
   wire          cmdout_tlast, cmdout_tvalid, cmdout_tready, ackin_tlast, ackin_tvalid, ackin_tready;
@@ -72,6 +69,12 @@ module noc_block_predistort #(
   wire str_sink_tlast, str_sink_tvalid, str_sink_tready;
 
   wire [63:0] str_src_tdata[0:NUM_CHANNELS-1];
+  wire [64*NUM_CHANNELS-1:0] str_src_tdata_flat;
+  genvar b;
+  generate
+    for (b = 0; b < NUM_CHANNELS; b = b + 1)
+       assign str_src_tdata_flat[64*(b+1)-1:64*b] = str_src_tdata[b];
+  endgenerate
   wire [NUM_CHANNELS-1:0] str_src_tlast, str_src_tvalid, str_src_tready;
 
   // AXI Wrapper
@@ -83,13 +86,13 @@ module noc_block_predistort #(
   // output (source) data
   wire [31:0]  out_tdata[0:NUM_CHANNELS-1]; //this syntax is fucked.
   wire [127:0] out_tuser[0:NUM_CHANNELS-1];
-  wire [127:0] out_tuser_pre[0:NUM_CHANNELS-1];
-  wire [NUM_CHANNELS-1:0] out_tlast, out_tvalid, out_tready;
+  //we define these as four-wide so we can assign to them in the max case (four channels)
+  wire [127:0] out_tuser_pre[0:3];
+  wire [3:0] out_tlast, out_tvalid, out_tready;
 
   //----------------------------------------------------------------------------
   // Registers
   //----------------------------------------------------------------------------
-
   // Readback register data
   reg [63:0] rb_data;
 
@@ -119,7 +122,7 @@ module noc_block_predistort #(
     .STR_SINK_FIFOSIZE(STR_SINK_FIFOSIZE),
     .INPUT_PORTS(1),
     .OUTPUT_PORTS(NUM_CHANNELS))
-  noc_shell_inst (
+  noc_shell (
     .bus_clk(bus_clk),
     .bus_rst(bus_rst),
     .i_tdata(i_tdata),
@@ -138,6 +141,8 @@ module noc_block_predistort #(
     .set_addr(set_addr),
     .set_stb(set_stb),
     .rb_data(rb_data),
+    .rb_stb({NUM_CHANNELS{1'b1}}),
+    .rb_addr(),
     // Control Source (unused)
     .cmdout_tdata(cmdout_tdata),
     .cmdout_tlast(cmdout_tlast),
@@ -147,23 +152,23 @@ module noc_block_predistort #(
     .ackin_tlast(ackin_tlast),
     .ackin_tvalid(ackin_tvalid),
     .ackin_tready(ackin_tready),
+    .resp_in_dst_sid(),
+    .resp_out_dst_sid(),
     // Stream Sink
     .str_sink_tdata(str_sink_tdata),
     .str_sink_tlast(str_sink_tlast),
     .str_sink_tvalid(str_sink_tvalid),
     .str_sink_tready(str_sink_tready),
     // Stream Sources //TODO ideally should be parameterized for NUM_CHANNELS
-    .str_src_tdata({str_src_tdata[3], str_src_tdata[2], str_src_tdata[1], str_src_tdata[0]}),
-    .str_src_tlast({str_src_tlast[3], str_src_tlast[2], str_src_tlast[1], str_src_tlast[0]}),
-    .str_src_tvalid({str_src_tvalid[3], str_src_tvalid[2], str_src_tvalid[1], str_src_tvalid[0]}),
-    .str_src_tready({str_src_tready[3], str_src_tready[2], str_src_tready[1], str_src_tready[0]}),
+    .str_src_tdata(str_src_tdata_flat),
+    .str_src_tlast(str_src_tlast),
+    .str_src_tvalid(str_src_tvalid),
+    .str_src_tready(str_src_tready),
     .clear_tx_seqnum(clear_tx_seqnum),
     .debug(debug));
 
   assign ackin_tready = 1'b1;
 
-  // AXI Wrapper - Convert RFNoC Shell interface into AXI stream interface
-  //
   wire [NUM_CHANNELS*32-1:0] taps_tdata_flat;
   (* mark_debug = "true" *) wire [15:0] taps_tdata[0:NUM_CHANNELS-1];
   (* mark_debug = "true" *) wire [NUM_CHANNELS-1:0] taps_tlast;
@@ -188,37 +193,36 @@ module noc_block_predistort #(
     end
   endgenerate
 
-  chdr_deframer deframer
+  chdr_deframer chdr_deframer
     (.clk(ce_clk), .reset(ce_rst), .clear(1'b0),
      .i_tdata(str_sink_tdata), .i_tlast(str_sink_tlast), .i_tvalid(str_sink_tvalid), .i_tready(str_sink_tready),
      .o_tdata(in_tdata), .o_tuser(in_tuser), .o_tlast(in_tlast), .o_tvalid(in_tvalid), .o_tready(in_tready));
 
-  //muck up your tuser streams
   //this causes a warning in synthesis because .o*_tlast is left blank
-  split_stream_fifo #(.WIDTH(128), .ACTIVE_MASK(4'b1111)) tuser_splitter (
-     .clk(ce_clk), .reset(ce_rst), .clear(1'b0),
-     .i_tdata(in_tuser), .i_tlast(1'b0), .i_tvalid(in_tvalid & in_tlast), .i_tready(),
-     .o0_tdata(out_tuser_pre[0]), .o0_tlast(), .o0_tvalid(), .o0_tready(out_tlast[0] & out_tready[0]),
-     .o1_tdata(out_tuser_pre[1]), .o1_tlast(), .o1_tvalid(), .o1_tready(out_tlast[1] & out_tready[1]),
-     .o2_tdata(out_tuser_pre[2]), .o2_tlast(), .o2_tvalid(), .o2_tready(out_tlast[2] & out_tready[2]),
-     .o3_tdata(out_tuser_pre[3]), .o3_tlast(), .o3_tvalid(), .o3_tready(out_tlast[3] & out_tready[3])
+  split_stream_fifo #(.WIDTH(128), .ACTIVE_MASK({NUM_CHANNELS{1'b1}})) tuser_splitter (
+       .clk(ce_clk), .reset(ce_rst), .clear(1'b0),
+       .i_tdata(in_tuser), .i_tlast(1'b0), .i_tvalid(in_tvalid & in_tlast), .i_tready(),
+       .o0_tdata(out_tuser_pre[0]), .o0_tlast(), .o0_tvalid(), .o0_tready(out_tlast[0] & out_tready[0]),
+       .o1_tdata(out_tuser_pre[1]), .o1_tlast(), .o1_tvalid(), .o1_tready(out_tlast[1] & out_tready[1]),
+       .o2_tdata(out_tuser_pre[2]), .o2_tlast(), .o2_tvalid(), .o2_tready(out_tlast[2] & out_tready[2]),
+       .o3_tdata(out_tuser_pre[3]), .o3_tlast(), .o3_tvalid(), .o3_tready(out_tlast[3] & out_tready[3])
   );
-  //ugh i don't know how to do this inside the generate loop (the 4'b0000 messes
-  //me up, i guess you can't do 4'dk?)
-  assign out_tuser[0] = { out_tuser_pre[0][127:96], out_tuser_pre[0][79:68], 4'd0, next_dst[0], out_tuser_pre[0][63:0] };
-  assign out_tuser[1] = { out_tuser_pre[1][127:96], out_tuser_pre[1][79:68], 4'd1, next_dst[1], out_tuser_pre[1][63:0] };
-  assign out_tuser[2] = { out_tuser_pre[2][127:96], out_tuser_pre[2][79:68], 4'd2, next_dst[2], out_tuser_pre[2][63:0] };
-  assign out_tuser[3] = { out_tuser_pre[3][127:96], out_tuser_pre[3][79:68], 4'd3, next_dst[3], out_tuser_pre[3][63:0] };
 
-  //DERP! the four predistorters are being driven off the same AXI stream!
-  //you'll want to split that stream into four streams. this is a lot of
-  //buffering! are you sure you need to do this? we'll try it without
-  //the FIFO and see if it passes timing.
+  //muck up your tuser streams
+  genvar z;
+  generate
+    for(z = 0; z < NUM_CHANNELS; z = z + 1) begin
+      wire [3:0] ch;
+      assign ch = z;
+      assign out_tuser[z] = { out_tuser_pre[z][127:96], out_tuser_pre[z][79:68], ch, next_dst[z], out_tuser_pre[z][63:0] };
+    end
+  endgenerate
 
-  wire [15:0] input_split_tdata[0:NUM_CHANNELS-1];
-  wire [NUM_CHANNELS-1:0] input_split_tlast, input_split_tvalid, input_split_tready;
 
-  split_stream #(.WIDTH(16), .ACTIVE_MASK(4'b1111)) input_splitter (
+  //you'll want to split that stream into four streams.
+  wire [15:0] input_split_tdata[0:3];
+  wire [3:0] input_split_tlast, input_split_tvalid, input_split_tready;
+  split_stream #(.WIDTH(16), .ACTIVE_MASK({NUM_CHANNELS{1'b1}})) input_splitter (
      .clk(ce_clk), .reset(ce_rst), .clear(1'b0),
      .i_tdata(in_tdata[15:0]), .i_tlast(in_tlast), .i_tvalid(in_tvalid), .i_tready(in_tready),
      .o0_tdata(input_split_tdata[0]), .o0_tlast(input_split_tlast[0]), .o0_tvalid(input_split_tvalid[0]), .o0_tready(input_split_tready[0]),
